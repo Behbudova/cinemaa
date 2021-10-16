15ffa85adf04ef02f87083b54b72cb0bb4142623
Behbudova <92176238+Behbudova@users.noreply.github.com>
2021-10-16 13:33:19 +0400

    Add files via upload
    design of cinema web page

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+const schemeSvg=document.querySelector('.scheme-svg')
+schemeSvg.addEventListener('click',(event)=>{
+    if( !event.target.classList.contains('booked')){
+        event.target.classList.toggle('active')
+    }
+
+
+    //event.target.classList.toggle('active')
+})
